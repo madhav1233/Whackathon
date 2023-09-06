9409c92dfc023eca26d21767e460baa11252193c
buckth3duck <madhav.hsejar@gmail.com>
2023-09-06 10:43:36 +0800

    node.js code created

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, Response
+from botbuilder.core import BotFrameworkAdapter, BotFrameworkAdapterSettings
+from botbuilder.schema import Activity
+from bot import DatabaseBot  # Import the bot from bot.py
+
+# Initialize Flask
+app = Flask(__name__)
+
+# Bot Framework Adapter settings
+SETTINGS = BotFrameworkAdapterSettings("cb1d782a-d2ea-48fb-90a5-75dcb6333844")
+ADAPTER = BotFrameworkAdapter(SETTINGS)
+
+# Initialize our bot
+BOT = DatabaseBot("tickets.db", "YOUR_OPENAI_API_KEY")
+
+@app.route("/api/messages", methods=["POST"])
+def messages():
+    if "application/json" in request.headers["Content-Type"]:
+        body = request.json
+    else:
+        return Response(status=415)
+
+    activity = Activity().deserialize(body)
+
+    async def call_bot(turn_context):
+        await BOT.on_turn(turn_context)
+
+    task = ADAPTER.process_activity(activity, "", call_bot)
+    return Response(status=201)
+
+if __name__ == "__main__":
+    app.run(port=3978)
---
+++ bot.py
@@ -0,0 +1,41 @@
+from botbuilder.core import ActivityHandler, TurnContext, MessageFactory
+from botbuilder.schema import ChannelAccount
+import sqlite3
+import openai
+
+class DatabaseBot(ActivityHandler):
+
+    def __init__(self, database_path, openai_api_key):
+        self.database_path = database_path
+        openai.api_key = openai_api_key
+
+    async def on_members_added_activity(self, members_added: [ChannelAccount], turn_context: TurnContext):
+        for member in members_added:
+            if member.id != turn_context.activity.recipient.id:
+                await turn_context.send_activity("Hello and welcome! Ask me anything about our data.")
+
+    async def on_message_activity(self, turn_context: TurnContext):
+        user_input = turn_context.activity.text
+        response = self.query_database(user_input)
+        await turn_context.send_activity(response)
+
+    def query_database(self, user_input: str) -> str:
+        # Generate SQL query using GPT-3
+        prompt = f"Translate the following user request into an SQL query: '{user_input}'"
+        response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=150)
+        query = response.choices[0].text.strip()
+
+        # Connect to the SQLite database
+        connection = sqlite3.connect(self.database_path)
+        cursor = connection.cursor()
+
+        # Execute the query and fetch results
+        try:
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            response = "\n".join([str(row) for row in rows])
+        except Exception as e:
+            response = f"Error executing query: {e}"
+
+        connection.close()
+        return response
